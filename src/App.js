0aa5a77885cd09a4d237d32f159d2700f590e2e4
Joy-LQ <1621251909@qq.com>
2024-11-21 22:55:29 +0000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import YouAndMeGame from './components/YouAndMeGame';
+
+function App() {
+  return (
+    <div className="App">
+      <YouAndMeGame />
+    </div>
+  );
+}
+
+export default App;
